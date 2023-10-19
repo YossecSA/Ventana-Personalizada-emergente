d4011c2bb310dca8becd2ec833fbb300fef51401
Yossec <yosec.arratea22@gmail.com>
2023-10-19 10:43:41 -0500

    popup emergente

---
+++ PopPud_prueba/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopPud_prueba
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var popup = new MiPopupPage();
+            await PopupNavigation.Instance.PushAsync(new MiPopupPage()); //para abrir el poppup
+        }
+    }
+}
---
+++ PopPud_prueba/MiPopupPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Animations;
+using Rg.Plugins.Popup.Enums;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopPud_prueba
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MiPopupPage : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public MiPopupPage()
+        {
+
+            InitializeComponent();
+
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            // Cerrar el popup
+            await Rg.Plugins.Popup.Services.PopupNavigation.Instance.PopAsync();
+
+        }
+    }
+}
